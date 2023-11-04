dcd7fd41abfaf922b162bbf6597793fabb71d28d
Joe Marron <marronjo@tcd.ie>
2023-11-04 09:53:18 +0000

    add cobra cli framework

---
+++ cmd/root.go
@@ -0,0 +1,38 @@
+// Package cmd /*
+package cmd
+
+import (
+	"github.com/spf13/cobra"
+	"os"
+)
+
+// rootCmd represents the base command when called without any subcommands
+var rootCmd = &cobra.Command{
+	Use:   "addgen",
+	Short: "CLI for generating crypto addresses",
+	Long: `A powerful CLI used to generate new/existing crypto addresses:
+
+It allows for addresses to be generated from an existing private key or
+new key generation for supported address types.`,
+}
+
+// Execute adds all child commands to the root command and sets flags appropriately.
+// This is called by main.main(). It only needs to happen once to the rootCmd.
+func Execute() {
+	err := rootCmd.Execute()
+	if err != nil {
+		os.Exit(1)
+	}
+}
+
+func init() {
+	// Here you will define your flags and configuration settings.
+	// Cobra supports persistent flags, which, if defined here,
+	// will be global for your application.
+
+	// rootCmd.PersistentFlags().StringVar(&cfgFile, "config", "", "config file (default is $HOME/.addgen.yaml)")
+
+	// Cobra also supports local flags, which will only run
+	// when this action is called directly.
+	rootCmd.Flags().BoolP("toggle", "t", false, "Help message for toggle")
+}
